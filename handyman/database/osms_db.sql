d047fe42f592616a6e19dbcd141801f9756ca89b
Aldi Hamati <110453529+aldihamati@users.noreply.github.com>
2023-05-22 12:07:46 +0200

    Add files via upload

---
+++ handyman/database/osms_db.sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 13, 2023 at 06:17 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `osms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adminlogin_tb`
+--
+
+CREATE TABLE `adminlogin_tb` (
+  `a_login_id` int(11) NOT NULL,
+  `a_name` varchar(60) COLLATE utf8_bin NOT NULL,
+  `a_email` varchar(60) COLLATE utf8_bin NOT NULL,
+  `a_password` varchar(60) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `adminlogin_tb`
+--
+
+INSERT INTO `adminlogin_tb` (`a_login_id`, `a_name`, `a_email`, `a_password`) VALUES
+(1, 'Admin', 'admin@gmail.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignwork_tb`
+--
+
+CREATE TABLE `assignwork_tb` (
+  `rno` int(11) NOT NULL,
+  `request_id` int(11) NOT NULL,
+  `request_info` text COLLATE utf8_bin NOT NULL,
+  `request_desc` text COLLATE utf8_bin NOT NULL,
+  `requester_name` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_add1` text COLLATE utf8_bin NOT NULL,
+  `requester_add2` text COLLATE utf8_bin NOT NULL,
+  `requester_city` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_state` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_zip` int(11) NOT NULL,
+  `requester_email` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_mobile` bigint(11) NOT NULL,
+  `assign_tech` varchar(60) COLLATE utf8_bin NOT NULL,
+  `assign_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `assignwork_tb`
+--
+
+INSERT INTO `assignwork_tb` (`rno`, `request_id`, `request_info`, `request_desc`, `requester_name`, `requester_add1`, `requester_add2`, `requester_city`, `requester_state`, `requester_zip`, `requester_email`, `requester_mobile`, `assign_tech`, `assign_date`) VALUES
+(16, 61, 'Fridge problems', 'The light does not work', 'Alesia Gjana', 'Durresit', 'Somewhere', 'Tirana', 'NA', 1004, 'alesiagjana@gmail.com', 683757839, 'John Doe', '2023-06-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requesterlogin_tb`
+--
+
+CREATE TABLE `requesterlogin_tb` (
+  `r_login_id` int(11) NOT NULL,
+  `r_name` varchar(60) COLLATE utf8_bin NOT NULL,
+  `r_email` varchar(60) COLLATE utf8_bin NOT NULL,
+  `r_password` varchar(60) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `requesterlogin_tb`
+--
+
+INSERT INTO `requesterlogin_tb` (`r_login_id`, `r_name`, `r_email`, `r_password`) VALUES
+(13, 'Alesia Gjana', 'alesiagjana@gmail.com', 'prog');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+CREATE TABLE `review` (
+  `name` varchar(100) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `message` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submitrequest_tb`
+--
+
+CREATE TABLE `submitrequest_tb` (
+  `request_id` int(11) NOT NULL,
+  `request_info` text COLLATE utf8_bin NOT NULL,
+  `request_desc` text COLLATE utf8_bin NOT NULL,
+  `requester_name` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_add1` text COLLATE utf8_bin NOT NULL,
+  `requester_add2` text COLLATE utf8_bin NOT NULL,
+  `requester_city` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_state` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_zip` int(11) NOT NULL,
+  `requester_email` varchar(60) COLLATE utf8_bin NOT NULL,
+  `requester_mobile` bigint(11) NOT NULL,
+  `request_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `submitrequest_tb`
+--
+
+INSERT INTO `submitrequest_tb` (`request_id`, `request_info`, `request_desc`, `requester_name`, `requester_add1`, `requester_add2`, `requester_city`, `requester_state`, `requester_zip`, `requester_email`, `requester_mobile`, `request_date`) VALUES
+(61, 'Fridge problems', 'The light does not work', 'Alesia Gjana', 'Durresit', 'Somewhere', 'Tirana', 'NA', 1004, 'alesiagjana@gmail.com', 683757839, '2023-06-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `technician_tb`
+--
+
+CREATE TABLE `technician_tb` (
+  `empid` int(11) NOT NULL,
+  `empName` varchar(60) COLLATE utf8_bin NOT NULL,
+  `empCity` varchar(60) COLLATE utf8_bin NOT NULL,
+  `empMobile` bigint(11) NOT NULL,
+  `empEmail` varchar(60) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `technician_tb`
+--
+
+INSERT INTO `technician_tb` (`empid`, `empName`, `empCity`, `empMobile`, `empEmail`) VALUES
+(14, 'John Doe', 'Tirana', 68795858, 'johndoe@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `adminlogin_tb`
+--
+ALTER TABLE `adminlogin_tb`
+  ADD PRIMARY KEY (`a_login_id`);
+
+--
+-- Indexes for table `assignwork_tb`
+--
+ALTER TABLE `assignwork_tb`
+  ADD PRIMARY KEY (`rno`);
+
+--
+-- Indexes for table `requesterlogin_tb`
+--
+ALTER TABLE `requesterlogin_tb`
+  ADD PRIMARY KEY (`r_login_id`);
+
+--
+-- Indexes for table `submitrequest_tb`
+--
+ALTER TABLE `submitrequest_tb`
+  ADD PRIMARY KEY (`request_id`);
+
+--
+-- Indexes for table `technician_tb`
+--
+ALTER TABLE `technician_tb`
+  ADD PRIMARY KEY (`empid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `adminlogin_tb`
+--
+ALTER TABLE `adminlogin_tb`
+  MODIFY `a_login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `assignwork_tb`
+--
+ALTER TABLE `assignwork_tb`
+  MODIFY `rno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `requesterlogin_tb`
+--
+ALTER TABLE `requesterlogin_tb`
+  MODIFY `r_login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `submitrequest_tb`
+--
+ALTER TABLE `submitrequest_tb`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+
+--
+-- AUTO_INCREMENT for table `technician_tb`
+--
+ALTER TABLE `technician_tb`
+  MODIFY `empid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
